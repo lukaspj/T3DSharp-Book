c5c20d04ab0574853c48e411a953ac541d01bdd6
Lukas Aldershaab <lpjoergensen@gmail.com>
2020-10-18 11:31:19 +0200

    Initial version

---
+++ README.md
@@ -0,0 +1,8 @@
+[![CC BY NC SA 4.0][cc-by-nc-sa-shield]][cc-by-nc-sa]
+
+
+
+# T3DSharp - Book
+
+This is a WIP for my T3D book.
+
---
+++ cleanup.bat
@@ -0,0 +1,17 @@
+del *.aux
+del *.bbl
+del *.bcf
+del *.blg
+del *.idx
+del *.ilg
+del *.ind
+del *.log
+del *.out
+del *.ptc
+del *.run.xml
+del *.toc
+del *.synctex.gz
+del *.glo
+del *.gls
+del *.glg
+del *.ist
